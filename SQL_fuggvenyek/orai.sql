c5651a3945b16624c5c08a8d1f1f45f4e0994458
Csongor00 <71691734+Csongor00@users.noreply.github.com>
2022-10-18 12:16:49 +0200

    Update orai.sql

--- SQL_fuggvenyek/orai.sql
+++ SQL_fuggvenyek/orai.sql
@@ -1,1 +1,5 @@
-
+--1. Feladat
+/*A 'products' táblában határozd meg a 'standard_cost' mezőjének átlagértékét
+a, az össezs rekord esetében*/
+USE northwind;
+SELECT AVG(standard_cost) as Átlag FROM products;
